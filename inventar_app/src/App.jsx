de8622cce5387048c31dec6e90c0f4e23a49b617
Copernicus12 <razvan.hosu32@gmail.com>
2025-01-07 15:09:49 +0200

    refactor App component to remove Login page and integrate items prop into routes; delete unused PrivateRoute and Login components

--- inventar_app/src/App.jsx
+++ inventar_app/src/App.jsx
@@ -10,7 +10,6 @@ import axios from 'axios';
 import Dashboard from './Pages/Dashboard';
 import Inventory from './Pages/Inventory';
 import Orders from './Pages/Orders';
-import Login from './Pages/Login';
 import Stock from './Pages/Stock';
 import RFID from './Pages/RFID';
 import Settings from './Pages/Settings';
@@ -29,38 +28,25 @@ function App() {
   }, []);
 
   return (
-      <Router>
-          <div className="App">
-              <Routes>
-                  {/* Login Route */}
-                  <Route path="/login" element={<Login />} />
-
-                  {/* Main Routes (For authenticated users) */}
-                  <Route
-                      path="/*"
-                      element={
-                          <>
-                              <AppHeader />
-                              <div className="SideMenuAndPageContent">
-                                  <SideMenu />
-                                  <div className="PageContent">
-                                      <Routes>
-                                          <Route path="/" element={<Dashboard />} />
-                                          <Route path="/inventory" element={<Inventory />} />
-                                          <Route path="/orders" element={<Orders />} />
-                                          <Route path="/stock" element={<Stock />} />
-                                          <Route path="/rfid" element={<RFID />} />
-                                          <Route path="/settings" element={<Settings />} />
-                                      </Routes>
-                                  </div>
-                              </div>
-                              <AppFooter />
-                          </>
-                      }
-                  />
-              </Routes>
+    <Router>
+      <div className="App">
+        <AppHeader />
+        <div className="SideMenuAndPageContent">
+          <SideMenu />
+          <div className="PageContent">
+            <Routes>
+              <Route path="/" element={<Dashboard items={items} />} />
+              <Route path="/inventory" element={<Inventory items={items} />} />
+              <Route path="/orders" element={<Orders items={items} />} />
+              <Route path="/stock" element={<Stock items={items} />} />
+              <Route path="/rfid" element={<RFID items={items} />} />
+              <Route path="/settings" element={<Settings items={items} />} />
+            </Routes>
           </div>
-      </Router>
+        </div>
+        <AppFooter />
+      </div>
+    </Router>
   );
 }
 
